66b9cf641d20e6ce3c26214eb04186371d664298
Bryan Ramirez-Gonzalez <bryanram2024@gmail.com>
2024-09-21 04:18:51 -0400

    adding firebase code

--- backEnd/app.py
+++ backEnd/app.py
@@ -1,14 +1,22 @@
 from flask import Flask, jsonify
+#from firebase_admin import credentials, firestore
+#import firebase_admin
+
 from flask_cors import CORS
+import os
+
+# File
+from firebase_config import create_stock_analysis, check_for_stock, read_stock_analysis, update_stock_analysis, delete_stock_analysis
 
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/api/data', methods=['GET'])
-def get_data():
-    # Example data to send to React
-    data = {'message': 'Hello from Flask!'}
-    return jsonify(data)
+@app.route('/')
+def home():
+    return jsonify({"message": "Hello, World!"})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+#@app.route('/stocks/parse/<ticker>')
+@app.route('/stock/<string:ticker>', methods=['GET'])
+
+def get_stock_data(ticker):
+    
